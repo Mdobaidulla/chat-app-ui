61e3b4c77b35b0b7f74b1c6daf00bc859c68bc3e
Md Obaidulla <mitul.li@yahoo.com>
2020-12-08 22:33:22 -0500

    First commit

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Layout extends Component{
+    render(){
+        return(
+            <>
+            All the layout will goes here
+            </>
+        )
+    }
+}
+
+export default Layout;
